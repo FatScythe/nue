f683e89a674c09bcb732a4114be01f1e58df4672
Abdullahi Fahm <holadhayo28@gmail.com>
2026-04-25 21:26:10 +0100

    feat: access token endpoints and caching strategy

--- apps/core-service/src/auth/auth.controller.ts
+++ apps/core-service/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { GetUser, NoToken } from '../common/decorator';
+import type { ReqUser } from '../common/types';
+import { AuthService } from './auth.service';
 
-@Controller()
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @NoToken()
+  @Get('access-token')
+  @HttpCode(HttpStatus.OK)
+  getAccessToken(@GetUser() user: ReqUser) {
+    return this.authService.getAccessToken(user);
+  }
+}
--- apps/core-service/src/auth/auth.service.ts
+++ apps/core-service/src/auth/auth.service.ts
@@ -1,6 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+
+import { ReqUser } from '../common/types';
+import { ApiException } from '../common/exception';
+import { ApiErrorCode } from '../common/enums';
+
+// workspace services
+import { AuthService as GAuthService } from '@auth/auth.service';
+import { REDIS_CLIENT } from '@database/redis.provider';
+
+import { Redis } from 'ioredis';
+import { configuration } from '../config';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  private readonly logger = new Logger(AuthService.name);
+
+  constructor(
+    private readonly globalAuthService: GAuthService,
+    @Inject(REDIS_CLIENT) private readonly redis: Redis,
+  ) {}
+
+  async getAccessToken(user: ReqUser) {
+    const userId = user.id;
+
+    if (!userId)
+      throw new ApiException(
+        ApiErrorCode.InternalServerError,
+        'GAT001: request could not be processed. try again later',
+        { error_code: 'GAT001' },
+      );
+
+    const expiresIn = 5 * 60;
+    const cacheKey = `accesstoken_${user.id}`;
+
+    try {
+      const cachedToken = await this.redis.get(cacheKey);
+
+      if (cachedToken) {
+        const ttl = await this.redis.ttl(cacheKey);
+
+        return {
+          message: `token will expire in ${Math.ceil(ttl / 60)} minutes`,
+          data: {
+            accessToken: cachedToken,
+            expiresIn: ttl > 0 ? ttl : expiresIn,
+          },
+        };
+      }
+    } catch (error) {
+      this.logger.error({ message: 'Error accessing redis cache', error });
+    }
+
+    const { access_token } = await this.globalAuthService.generateToken(
+      {
+        sub: userId,
+      },
+      { expiresIn },
+    );
+
+    try {
+      await this.redis.setex(cacheKey, expiresIn, access_token);
+    } catch (error) {
+      this.logger.error({ message: 'Error setting redis cache', error });
+    }
+
+    return {
+      message: `token will expire in ${expiresIn / 60} minutes`,
+      data: { accessToken: access_token, expiresIn: expiresIn },
+    };
+  }
 }
